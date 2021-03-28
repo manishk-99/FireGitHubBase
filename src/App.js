12d49c152409ebd74545fb9834460d097720e74c
Manish Kumar <66010479+manishk-99@users.noreply.github.com>
2021-03-28 14:40:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./App.css";
+
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//Routing
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//Toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+//FireBase
+import firebase from "firebase/app";
+import "firebase/auth";
+
+//Components
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import PageNotFound from "./Pages/PageNotFound";
+import { UserContext } from "./Context/UserContext";
+import Footer from "./Layout/footer";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Signin" component={Signin} />
+          <Route exact path="/Signup" component={Signup} />
+          <Route exact path="*" component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
+  );
+};
+
+export default App;
