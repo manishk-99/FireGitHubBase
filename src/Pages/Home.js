229a371705e7edeb311e9856560807d73234e177
Manish Kumar <66010479+manishk-99@users.noreply.github.com>
2021-03-30 15:22:25 +0530

    HomePage-done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import Axios from "axios";
+
+import {
+  Row,
+  Container,
+  Col,
+  Input,
+  Button,
+  InputGroup,
+  InputGroupAddon
+} from "reactstrap";
+
+import UserCard from "../components/UserCard";
+import Repos from "../components/Repos";
+import { Redirect } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { toast } from "react-toastify";
 
 const Home = () => {
+  const context = useContext(UserContext);
+  const [query, setQuery] = useState("");
+  const [user, setUser] = useState(null);
+
+  const fetchDetails = async () => {
+    try {
+      const { data } = await Axios.get(`https://api.github.com/users/${query}`);
+      setUser(data);
+    } catch (error) {
+      toast("Not able to locate user", { type: "error" });
+    }
+  };
   return (
-    <div>
-      <h1>Home Page</h1>;
-    </div>
+    <Container>
+      <Row className=" mt-3">
+        <Col md="5">
+          <InputGroup>
+            <Input
+              type="text"
+              value=""
+              placeholder="Please provide the username"
+            />
+            <InputGroupAddon addonType="append">
+              <Button color="primary">Fetch User</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Col>
+        <Col md="7"></Col>
+      </Row>
+    </Container>
   );
 };
-
 export default Home;
